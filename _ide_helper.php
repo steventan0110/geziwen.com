bff37b5dd090c0ce655b69afbc85ed554098b92f
GodfatherSean <1078351005@qq.com>
2018-02-28 18:00:08 +0800

    Add IDE Helper

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.6 on 2018-02-28 09:53:27.
+ * Generated for Laravel 5.6.6 on 2018-02-28 09:56:08.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
