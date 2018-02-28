5e7f04182e71e2a605871bdd2128f279aae0c6a7
GodfatherSean <1078351005@qq.com>
2018-03-01 02:45:24 +0800

    Applicant Controller & Table

---
+++ app/Http/Controllers/ApplicantController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApplicantController extends Controller
+{
+    public function profile() {
+
+    }
+
+    public function create() {
+
+    }
+
+    public function edit() {
+
+    }
+}
+
+// TODO: Implement this file
