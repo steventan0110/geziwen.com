5d6c4403d5fc045efbe02901247c063cac5558c1
GodfatherSean <1078351005@qq.com>
2018-03-01 21:38:49 +0800

    Add Activities Table

---
+++ database/migrations/2018_03_01_132419_create_activities_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('student');
+            $table->unsignedInteger('type');
+            $table->text('name');
+            $table->text('description');
+            $table->date('start');
+            $table->date('end');
+            $table->unsignedInteger('hours_per_week');
+            $table->timestamps();
+        });
+
+        Schema::create('activity_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+        Schema::dropIfExists('activity_types');
+    }
+}
