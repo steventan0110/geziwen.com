0f89bcc845bff13eabc980c3b635a6e98ca04931
GodfatherSean <1078351005@qq.com>
2018-03-01 21:39:02 +0800

    Add Rewards Table

---
+++ database/migrations/2018_03_01_133123_create_awards_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAwardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('awards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('student');
+            $table->unsignedInteger('type');
+            $table->string('name');
+            $table->string('description');
+            $table->date('received_on');
+            $table->timestamps();
+        });
+
+        Schema::create('award_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('awards');
+        Schema::dropIfExists('award_types');
+    }
+}
