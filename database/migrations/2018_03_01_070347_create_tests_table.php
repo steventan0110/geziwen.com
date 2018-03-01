572525ac9694cb03220248c03019f3b9359bcd84
GodfatherSean <1078351005@qq.com>
2018-03-01 21:36:11 +0800

    Add Tests Table

--- database/migrations/2018_03_01_070347_create_tests_table.php
+++ database/migrations/2018_03_01_070347_create_tests_table.php
@@ -11,10 +11,54 @@ class CreateTestsTable extends Migration
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('tests', function (Blueprint $table) {
+    public function up() {
+        Schema::create('toefl', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('student');
+            $table->date('taken_on');
+            $table->unsignedInteger('reading');
+            $table->unsignedInteger('listening');
+            $table->unsignedInteger('speaking');
+            $table->unsignedInteger('writing');
+            $table->timestamps();
+        });
+
+        Schema::create('ielts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('student');
+            $table->date('taken_on');
+            $table->unsignedInteger('reading');
+            $table->unsignedInteger('listening');
+            $table->unsignedInteger('speaking');
+            $table->unsignedInteger('writing');
+            $table->timestamps();
+        });
+
+        Schema::create('sat', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('student');
+            $table->date('taken_on');
+            $table->unsignedInteger('reading');
+            $table->unsignedInteger('math');
+            $table->unsignedInteger('essay');
+            $table->timestamps();
+        });
+
+        Schema::create('sat_subject', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('student');
+            $table->date('taken_on');
+            $table->string('subject');
+            $table->unsignedInteger('score');
+            $table->timestamps();
+        });
+
+        Schema::create('ap', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('student');
+            $table->date('taken_on');
+            $table->string('subject');
+            $table->unsignedInteger('score');
             $table->timestamps();
         });
     }
@@ -26,6 +70,10 @@ class CreateTestsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tests');
+        Schema::dropIfExists('toefl');
+        Schema::dropIfExists('ielts');
+        Schema::dropIfExists('sat_subject');
+        Schema::dropIfExists('sat');
+        Schema::dropIfExists('ap');
     }
 }
