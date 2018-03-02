763c540b4ba150c6e83c5ac584cfd5c957b5183d
GodfatherSean <1078351005@qq.com>
2018-03-02 15:13:38 +0800

    New View & Component

---
+++ resources/assets/js/components/Tests/ToeflComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default" style="margin-bottom: 1rem!important;">
+                    <div class="card-header">Profile</div>
+                    <div class="card-body"></div>
+                </div>
+                <div class="card card-default" style="margin-bottom: 1rem!important;">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    "use strict";
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
