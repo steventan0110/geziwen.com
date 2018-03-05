d3e994bd02e9bbcfcce98b22101849100bbd2598
GodfatherSean <1078351005@qq.com>
2018-03-05 10:15:48 +0800

    Agency table migration

---
+++ database/migrations/2018_03_05_020333_create_agencies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAgenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agencies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('introduction');
+            $table->string('address');
+            $table->string('telephone');
+            $table->string('website');
+            $table->date('started_on');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agencies');
+    }
+}
