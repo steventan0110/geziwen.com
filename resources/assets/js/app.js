9ee388df798189ab51add95add0f1b5e2c3cb0f8
GodfatherSean <1078351005@qq.com>
2018-03-13 12:54:04 +0800

    Register Vue Material

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,7 +17,10 @@ window.Vue = require('vue');
  */
 
 import Avatar from 'vue-avatar'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
 
+Vue.use(VueMaterial);
 Vue.component('Avatar', Avatar);
 
 const app = new Vue({
