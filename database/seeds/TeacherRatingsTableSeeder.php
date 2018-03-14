fbfc65b8675bcfda7828c08e539ad0cbb80abf4c
fmx <1417388068@qq.com>
2018-03-14 16:26:11 +0800

    create databases for Ratings and fake its data

---
+++ database/seeds/TeacherRatingsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 2018/3/14
+ * Time: 16:01
+ */
+
+use Illuminate\Database\Seeder;
+
+class TeacherRatingsTableSeeder extends Seeder {
+
+    public function run() {
+
+        $faker = Faker\Factory::create();
+
+        foreach (range(1, 100) as $i) {
+            DB::table('teacher_comments')->insert([
+                'teacher_name' => $faker->name,
+                'teacher_ratings' => random_int(1, 10),
+                'teacher_id' => random_int(1, 50)
+            ]);
+        }
+    }
+}
+
+
