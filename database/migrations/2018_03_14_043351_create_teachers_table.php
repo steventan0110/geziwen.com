9574c580c881a9264885d7c1d8e44d84fe5ce42e
fmx <1417388068@qq.com>
2018-03-14 12:37:28 +0800

    Teachers_branch update

---
+++ database/migrations/2018_03_14_043351_create_teachers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->text('subject');
+            $table->mediumText('introduction');
+            $table->unsignedInteger('years_of_teaching');
+            $table->string('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
