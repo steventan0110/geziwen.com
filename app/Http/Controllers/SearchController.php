c146c1d00f6ce9d9744b04187a521f21f4a904c0
Zehao Huang <zehao.sean.huang@zehaohuang.cn>
2018-03-20 22:53:32 +0800

    Add Search Controller

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Agency\Agency;
+use App\Agency\Service\Plan;
+use App\Applicant\Applicant;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function result(Request $request) {
+//        TODO: Look into
+    }
+}
