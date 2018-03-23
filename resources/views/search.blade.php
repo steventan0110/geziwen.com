9872e9523f14f07da40ff73678f70d33892b3b94
Zehao Huang <zehao.sean.huang@zehaohuang.cn>
2018-03-23 18:21:25 +0800

    Search Blade Template

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,27 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-
     <div class="container">
-        <ais-index app-id="{{ config('scout.algolia.id') }}"
-                   api-key="{{ config('scout.algolia.search') }}"
-                   index-name="applicants_index">
-            <h1>请输入关键字进行搜索</h1>
-            <ais-input placeholder="Search contacts..." class="form-control"></ais-input>
+        <div class="justify-content-center">
+            <div class="card card-default">
+                <div class="card-header">
+                    <ul class="nav nav-tabs card-header-tabs pull-right" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#search-agencies" role="tab" aria-controls="search-agencies" aria-selected="false">中介</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#search-applicants" role="tab" aria-controls="search-applicants" aria-selected="true">案例</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#search-teachers" role="tab" aria-controls="search-teachers" aria-selected="false">师资</a>
+                        </li>
+                    </ul>
+                </div>
 
-            <ais-results>
-                <template scope="{ result }">
-                    <div>
-                        <h1>@{{ result.surname }}</h1>
-                        <h4>@{{ result.agency.name }} - @{{ result.agency.introduction }}</h4>
-                        <ul>
-                            <li>@{{ result.email }}</li>
-                        </ul>
+                <div class="card-body">
+                    <div class="tab-content text-left">
+                        <div class="tab-pane fade show active" id="search-agencies">
+                            <search algolia-app-id="{{ config('scout.algolia.id') }}"
+                                    algolia-api-key="{{ config('scout.algolia.search') }}"
+                                    algolia-index="agencies_index"></search>
+                        </div>
+                        <div class="tab-pane fade" id="search-applicants">
+                            <search algolia-app-id="{{ config('scout.algolia.id') }}"
+                                    algolia-api-key="{{ config('scout.algolia.search') }}"
+                                    algolia-index="applicants_index"></search>
+                        </div>
+                        <div class="tab-pane fade" id="search-teachers">
+                            <search algolia-app-id="{{ config('scout.algolia.id') }}"
+                                    algolia-api-key="{{ config('scout.algolia.search') }}"
+                                    algolia-index="teachers_index"></search>
+                        </div>
                     </div>
-                </template>
-            </ais-results>
-
-        </ais-index>
+                </div>
+            </div>
+        </div>
     </div>
 
 @endsection
