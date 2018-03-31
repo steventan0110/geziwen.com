2999f71e1a50ddd03b4aaa1ca8cf788d21cc6beb
fmx <1417388068@qq.com>
2018-03-31 18:08:26 +0800

    Refine store/get/delete method

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -16,55 +16,28 @@ use Illuminate\Support\Facades\DB;
 
 class CommentController extends Controller
 {
-    public function storeTeacherComment(Request $request) {
+    public function storeComment(Request $request) {
         $comment = new Comment;
         $comment->body = $request->body;
         $comment->commentable_id = $request->commentable_id;
-        $comment->commentable_type = 'teachers';
+        $comment->commentable_type = $request->commentable_type;
+        $comment->username = $request->username;
         $comment->rate = $request->rate;
         $comment->save();
     }
 
-    public function deleteTeacherComment(Request $request) {
+    public function deleteComment(Request $request) {
         $id = $request->id;
         Comment::where('id', $id)->delete();
     }
 
-    public function getTeacherComment(Request $request) {
+    public function getComment(Request $request) {
+        $type = $request->commentable_type;
         $comment_user = array();
         $comment_body = array();
         $rate_arr = array();
         $comment_id = array();
-        $comments = DB::select('select * from comments where commentable_type = ?', ['teachers']);
-        foreach ($comments as $comment) {
-            array_push($comment_body,$comment->body);
-            array_push($comment_user,$comment->username);
-            array_push($rate_arr,$comment->rate);
-            array_push($comment_id,$comment->id);
-        }
-        return array($comment_user,$comment_body,$rate_arr,$comment_id);
-    }
-
-    public function storeAgencyComment(Request $request) {
-        $comment = new Comment;
-        $comment->body = $request->body;
-        $comment->commentable_id = $request->commentable_id;
-        $comment->commentable_type = 'agencies';
-        $comment->rate = $request->rate;
-        $comment->save();
-    }
-
-    public function deleteAgencyComment(Request $request) {
-        $id = $request->id;
-        Comment::where('id', $id)->delete();
-    }
-
-    public function getAgencyComment(Request $request) {
-        $comment_user = array();
-        $comment_body = array();
-        $rate_arr = array();
-        $comment_id = array();
-        $comments = DB::select('select * from comments where commentable_type = ?', ['agencies']);
+        $comments = DB::select('select * from comments where commentable_type = ?', [$type]);
         foreach ($comments as $comment) {
             array_push($comment_body,$comment->body);
             array_push($comment_user,$comment->username);
