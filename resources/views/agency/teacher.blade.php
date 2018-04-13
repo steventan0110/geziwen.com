96784d7a0a8f110d3f81426779ca673c2d52ce7d
fmx <1417388068@qq.com>
2018-04-13 14:07:27 +0800

    Fix the problem when no comment exists

--- resources/views/agency/teacher.blade.php
+++ resources/views/agency/teacher.blade.php
@@ -5,32 +5,26 @@
 @endsection
 @section('content')
     {{-- TODO: Implement this view --}}
-    <div class="container" id="profile">
+    <div class="container" id="teacher">
         <div class="jumbotron bg-white box-shadow">
-            <div class="media row">
-                <img class="ml-2 mr-3 img-thumbnail img-responsive border border-info" src="https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/tam-ogel/a03f306fc98a8e119dae9d7c5510b656_121_121.jpg" alt="Generic placeholder image" width="100px">
-                <div class="media-body mt-4">
-                    <strong class="d-block text-gray-dark text-primary"><h2>{{ $teacher->name }}</h2></strong>
-                </div>
-            </div>
             <div class="media">
-                <div class="media-body">
-                    <p class="mt-4" ><p>教学科目:</p> {{ $teacher->introduction }}</p>
-                    <p class="mt-4"><p>所属中介:</p> {{ $teacher->name }}</p>
-
+                <img class="ml-2 mr-3 img-thumbnail border border-info rounded-circle" src="https://ss0.bdstatic.com/-0U0bnSm1A5BphGlnYG/tam-ogel/a03f306fc98a8e119dae9d7c5510b656_121_121.jpg" alt="Generic placeholder image" width="100px">
+                <div class="media-body ml-3">
+                    <h3>{{ $teacher->name }}</h3>
+                    <p class="small mt-3">所属中介: {{ $teacher->agency->name }}</p>
                 </div>
             </div>
-            <rating :my-comment-type="'{{ $teacher->comments[0]->commentable_type }}'"
-                    :my-comment-index="'{{ $teacher->comments[0]->commentable_id }}'"
+            <rating :my-comment-type="'teachers'"
+                    :my-comment-index="'{{ $teacher->id }}'"
+                    class="mt-5 mt-lg-0"
             >
             </rating>
             <hr class="my-4">
             <p class="small"><i class="mr-2 fas fa-phone"></i>{{ $teacher->agency->telephone }}</p>
             <p class="small"><i class="mr-2 fas fa-envelope"></i>{{ $teacher->agency->email }}</p>
+
         </div>
-
     </div>
-
     <div id="applicants" class="container mb-4">
         <div class="my-3 p-3 bg-white rounded box-shadow">
             <h5 class="border-bottom border-gray pb-2 mb-0">部分案例</h5>
@@ -47,17 +41,14 @@
 
     <div class="container" id="comments">
         <div class="my-3 p-3 bg-white rounded box-shadow">
-            <h5 class="border-bottom border-gray pb-2 mb-2">评论</h5>
             @auth
-                <comment-text :comment-data="'{{ $teacher->id }}'"
-                              :user-name="'{{ $user->name }}'"
-                              :comment-type="'{{ $teacher->comments[0]->commentable_type }}'"
-                              :comment-index="'{{ $teacher->comments[0]->commentable_id }}'"></comment-text>
+                <comment :user-name="'{{ Auth::user()->name }}'"
+                              :comment-type="'teachers'"
+                              :comment-index="'{{ $teacher->id }}'"></comment>
             @else
-                <comment-text :comment-data="'{{ $teacher->id }}'"
-                              :user-name="''"
-                              :comment-type="'{{ $teacher->comments[0]->commentable_type }}'"
-                              :comment-index="'{{ $teacher->comments[0]->commentable_id }}'"></comment-text>
+                <comment :user-name="''"
+                              :comment-type="'teachers'"
+                              :comment-index="'{{ $teacher->id }}'"></comment>
             @endauth
         </div>
     </div>
