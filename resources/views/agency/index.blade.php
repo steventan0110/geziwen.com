5d626b0bc26ce2136041aef03420c44a29762803
Zehao Huang <1078351005@qq.com>
2018-05-22 11:03:56 +0800

    Restore To Default Thumbnail

--- resources/views/agency/index.blade.php
+++ resources/views/agency/index.blade.php
@@ -23,7 +23,7 @@
             @foreach($agencies as $agency)
                 <div class="col-md-4">
                     <div class="card mb-4 box-shadow">
-                        <img class="card-img-top" src="https://ss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=629751542,2184084731&fm=202&mola=new&crop=v1" alt="logo" style="height: 225px; width: 100%; display: block;">
+                        <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22399%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20399%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16381e42e02%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A20pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16381e42e02%22%3E%3Crect%20width%3D%22399%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22132.109375%22%20y%3D%22121.35%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="logo">
                         <div class="card-body">
                             <h5>{{ $agency->name }}</h5>
                             <p class="card-text">{{ $agency->introduction }}</p>
