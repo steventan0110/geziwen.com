d413f1ba56cd3660200f80d6b5ab964eaf0b7fd7
fmx <1417388068@qq.com>
2018-05-29 20:59:30 +0800

    Add mobile-password login method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,62 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function validator(array $data)
+    {
+
+        return Validator::make($data, [
+            'username' => 'max:255',
+            'email' => 'email|max:255|unique:admin_users',
+            'mobile' => 'unique:admin_users',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $this->validate($request, [
+            'mobile' => 'string',
+            'email' => 'string',
+            'password' => 'required|string',
+        ]);
+    }
+
+    protected function credentials(Request $request)
+    {
+        if($request->only('email')!=null){
+            session(['type'=>'email']);
+            return $request->only('email', 'password');
+        }
+        else {
+            session(['type'=>'mobile']);
+            return $request->only('mobile', 'password');
+        }
+    }
+
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+
+        if(array_key_exists('email',$request->all())){
+            throw ValidationException::withMessages([
+                'email' => '邮箱与密码不匹配',
+            ]);
+        }
+        else {
+            throw ValidationException::withMessages([
+                'mobile' => '手机号与密码不匹配',
+            ]);
+        }
     }
 }
