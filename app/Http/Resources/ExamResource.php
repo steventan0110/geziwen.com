d4e01ed650acd1f4500873556a571555ea4215eb
fmx <1417388068@qq.com>
2018-05-31 16:10:10 +0800

    Add api resources

---
+++ app/Http/Resources/ExamResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ExamResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'type' => $this->type,
+            'score' => $this->score,
+            'remark' => $this->remark,
+            'taken_on' => $this->taken_on,
+        ];
+    }
+}
