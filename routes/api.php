af956a08da0b4399c91afeacb046d3dfbfd916cc
fmx <1417388068@qq.com>
2018-05-31 16:13:25 +0800

    Core api routes that return data in json

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\Agency\Agency;
+use App\Agency\Teacher;
+use App\Applicant\Applicant;
+use App\Http\Resources\ApplicantResource;
+use App\Http\Resources\TeacherResource;
+use App\Http\Resources\AgencyResource;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,3 +28,24 @@ Route::post('sms/send','SmsController@send');
 Route::post('sms/test','SmsController@test');
 Route::post('mail/send','MailController@send');
 Route::post('mail/test','MailController@test');
+
+Route::get('agencies', function() {
+    return AgencyResource::collection(Agency::paginate(5));
+});
+Route::get('agencies/{agency_id}', function($agency_id) {
+    return new AgencyResource(Agency::find($agency_id));
+});
+Route::get('teacher/{teacher_id}', function($teacher_id) {
+    return new TeacherResource(Teacher::find($teacher_id));
+});
+Route::get('agency/{agency_id}/teachers', function($agency_id) {
+
+    return TeacherResource::collection(Agency::find($agency_id)->teachers()->paginate(5));
+});
+Route::get('applicant/{applicant_id}', function($applicant_id) {
+    return new ApplicantResource(Applicant::find($applicant_id));
+});
+Route::get('agency/{agency_id}/applicants', function($agency_id) {
+    return ApplicantResource::collection(Agency::find($agency_id)->applicants()->paginate(5));
+});
+
